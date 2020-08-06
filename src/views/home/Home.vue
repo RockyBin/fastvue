08922aaa8a9ba09bfc0dcd49c2ba628e441a72cb
hwb <serverking6@163.com>
2020-08-06 17:30:08 +0800

    自定义组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,31 @@
 <template>
     <div class="index">
-        home
+        <NavigatorBar :title="$t(title)" @touchLeft="left" @touchRight="right" :custom="true" :customTitle="true" :size="20" :dot="true" :color="color"></NavigatorBar>  
+        <Grid :customContent="true" :imgs="imgs"></Grid>
+        <van-swipe-cell v-for="(item,index) in goods" :key="index">
+            <Card 
+            :customTags="true" 
+            :customFoot="true"
+            :price="item.price"
+            :originPrice="item.originPrice"
+            :desc="item.desc"
+            :thumb="item.thumb"
+            :tag="item.tag"
+            ></Card>
+            <template #right>
+                <Button :square="true"  type="danger" text="删除" />
+            </template>
+        </van-swipe-cell>
+
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import NavigatorBar from '@/components/NavBar/NavBar'
+import Grid from '@/components/Grid/Grid'
+import Card from '@/components/Card/Card'
+import Button from '@/components/Button/Button'
 
 export default {
     mounted(){
@@ -13,6 +33,38 @@ export default {
             console.log(res);
         })
     },
+    data(){
+        return {
+            title: 'home.home',
+            icon: 'point-gift-o',
+            color:'#323233',
+            imgs:[
+                {title:'',src:'https://img.yzcdn.cn/vant/apple-1.jpg',url:'http://www.baidu.com',to:'/dashboard/category'},
+                {title:'',src:'https://img.yzcdn.cn/vant/apple-2.jpg',url:'http://www.baidu.com',to:'/dashboard/category'},
+                {title:'',src:'https://img.yzcdn.cn/vant/apple-3.jpg',url:'http://www.baidu.com',to:'/dashboard/category'},
+            ],
+            goods:[
+                {title:'iphone10',desc:'爱疯手机，你的最爱！',price:6500,originPrice:8500,thumb:'https://img.yzcdn.cn/vant/ipad.jpeg',tag:''},
+                {title:'iphone11',desc:'爱疯手机，你的最爱！',price:7500,originPrice:9500,thumb:'https://img.yzcdn.cn/vant/cat.jpeg',tag:''},
+                {title:'iphone12',desc:'爱疯手机，你的最爱！',price:8500,originPrice:10500,thumb:'https://img.yzcdn.cn/vant/ipad.jpeg',tag:''},
+                {title:'iphone13',desc:'爱疯手机，你的最爱！',price:9500,originPrice:11500,thumb:'https://img.yzcdn.cn/vant/cat.jpeg',tag:''},
+            ]
+        }
+    },
+    methods:{
+        left(){
+            console.log('backup left');
+        },
+        right(){
+            console.log('touch right');
+        }
+    },
+    components:{
+        NavigatorBar,
+        Grid,
+        Card,
+        Button
+    }
 }
 </script>
 
